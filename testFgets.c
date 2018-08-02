3152f3e492663f0ab32ec9b7dc6c472d751e8a59
happy-wy <42026179+happy-wy@users.noreply.github.com>
2018-08-02 18:06:28 +0800

    Create testFgets.c

---
+++ testFgets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#define N 100
+
+int main() {
+	FILE *fp;
+	char s[N+1];
+	
+	if((fp=fopen("hello.txt","rt"))==NULL) {
+		printf("Cannot open file, press any key to exit!\n");
+		getch();
+    	exit(1);
+	}
+	
+    while(fgets(s,N,fp) != NULL) {     //读取字符串 
+    	printf("%s",s);
+	}
+	
+	fclose(fp);
+	system("pause");
+	return 0;
+}
